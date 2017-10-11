821bd72c25cfef88ce255ca37383629048e801df
bpatel3 <32682085+bpatel3@users.noreply.github.com>
2017-10-11 13:39:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('This class is dumb\n');
+  res.end('hello\n');
 });
 
 server.listen(port, hostname, () => {
